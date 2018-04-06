7dd106414eec223a0498bb1087c13787bbb4350b
MGlauer <martinglauer89@googlemail.com>
2018-04-06 13:02:38 +0200

    Add initial datastructures

--- parser.py
+++ parser.py
@@ -1,20 +1,83 @@
 import pyparsing as pp
 
 
-identifier = pp.Word(pp.alphanums+'_')
-
-variable = pp.Word(pp.alphanums.upper(), bodyChars=pp.alphanums+'_')
-
-functor = pp.Word(pp.alphanums.lower()+'_') ^ '\'' + pp.Word(pp.alphanums.lower()+'_') + '\''
-binary_connective = pp.Literal('|') | pp.Literal('&') | pp.Literal('<=>') \
-                    | pp.Literal('=>') | pp.Literal('<=') | pp.Literal('<~>') \
-                    | pp.Literal('~&') | pp.Literal('~|') | pp.Literal('!=') | pp.Literal('=')
-
-unary_connective = pp.Literal('~')
-
 fof_formula = pp.Forward()
 fof_plain_term = pp.Forward()
 fof_term = pp.Forward()
+
+class Identifier:
+    def __init__(self, *args):
+        self.name = args[0]
+identifier = pp.Word(pp.alphanums+'_').setParseAction(Identifier)
+
+
+class Variable:
+    def __init__(self, *args):
+        self.name = args[0]
+
+variable = pp.Word(pp.alphanums.upper(), bodyChars=pp.alphanums+'_').setParseAction(Variable)
+
+
+class Functor:
+    def __init__(self, *args):
+
+functor = pp.Word(pp.alphanums.lower()+'_') ^ '\'' + pp.Word(pp.alphanums.lower()+'_') + '\''
+
+
+class BinaryConnective:
+
+    DISJUNCTION = 0
+    CONJUNCTION = 1
+    BIIMPLICATION = 2
+    IMPLICATION = 3
+    REVERSE_IMPLICATION = 4
+    XOR = 5
+    NAND = 6
+    NOR = 7
+    NEQ = 8
+    EQUALS = 9
+
+    def __init__(self, *args):
+        symb = args[0]
+        if symb == '|':
+            self.kind = BinaryConnective.DISJUNCTION
+        elif symb == '&':
+            self.kind = BinaryConnective.CONJUNCTION
+        elif symb == '<=>':
+            self.kind = BinaryConnective.BIIMPLICATION
+        elif symb == '=>':
+            self.kind = BinaryConnective.IMPLICATION
+        elif symb == '<=':
+            self.kind = BinaryConnective.REVERSE_IMPLICATION
+        elif symb == '<~>':
+            self.kind = BinaryConnective.XOR
+        elif symb == '~&':
+            self.kind = BinaryConnective.NAND
+        elif symb == '~|':
+            self.kind = BinaryConnective.NOR
+        elif symb == '!=':
+            self.kind = BinaryConnective.NEQ
+        elif symb == '=':
+            self.kind = BinaryConnective.EQUALS
+        else:
+            raise NotImplementedError
+
+binary_connective = ( pp.Literal('|')  | pp.Literal('&')  | pp.Literal('<=>')
+                    | pp.Literal('=>') | pp.Literal('<=') | pp.Literal('<~>')
+                    | pp.Literal('~&') | pp.Literal('~|') | pp.Literal('!=')
+                    | pp.Literal('=')).setParseAction(BinaryConnective)
+
+class UnaryConnective:
+    NEGATION = 0
+
+    def __init__(self, *args):
+        symb = args[0]
+        if symb == '~':
+            self.kind = UnaryConnective.NEGATION
+
+unary_connective = pp.Literal('~')
+
+
 fof_atomic_formula = ('$' + fof_plain_term) ^ fof_plain_term ^ fof_term + '=' + fof_term
 fof_function_term = ('$' + fof_plain_term) ^ fof_plain_term
 fof_term << (fof_function_term ^ variable)
